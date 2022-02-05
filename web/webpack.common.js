a16506f5cff81fe793f0feefb48a51867789b06b
An Tran <antranprm@gmail.com>
2022-02-04 20:38:18 -0800

    Reconfigure

---
+++ web/webpack.common.js
@@ -0,0 +1,81 @@
+const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: {
+        index: "./src/index.ts",
+    },
+    output: {
+        filename: "[name].bundle.js",
+        chunkFilename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "",
+        assetModuleFilename: "./assets/[name][ext][query]"
+    },
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM",
+        "typed.js": "Typed"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            // favicon: './src/assets/favicon.ico',
+            scriptLoading: "defer",
+            chunks: ["index"],
+        }),
+    ],
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".scss", ".css", ".html"]
+    },
+    module: {
+        parser: {
+            javascript: {
+                url: "relative",
+            },
+        },
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.tsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "ts-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    // fallback to style-loader in development
+                    process.env.NODE_ENV !== 'production'
+                        ? 'style-loader'
+                        : MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    // fallback to style-loader in development
+                    process.env.NODE_ENV !== 'production'
+                        ? 'style-loader'
+                        : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+                type: 'asset/resource',
+            },
+        ]
+    },
+    optimization: {
+        splitChunks: {
+            chunks: "all",
+        },
+    },
+};
